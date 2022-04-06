6a00daf4a5146e9009f8220b401174fbd7b678ff
Szymon Mazur <szymon.mazur556@gmail.com>
2022-04-06 20:12:54 +0200

    pierwszy commit do zad 1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "multiply.h"
+
+using namespace std;
+
+int main()
+{
+std::cout<<"mnozenie "<<multiply(8,8)<<" teraz na zmienno przecinkowych  "<<multiply(8.5f,3.4f)<<endl;
+    return 0;
+}
---
+++ multiply.cpp
@@ -0,0 +1,8 @@
+#include "multiply.h"
+
+int multiply(int x, int y) {
+	return x * y;
+}
+float multiply(float x, float y) {
+	return x * y;
+}
